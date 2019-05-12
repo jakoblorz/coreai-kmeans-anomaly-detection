c30cbc3279803cf50ef36b57f311a6ef6365a316
jakob.lorz <jakob@lorz.org>
2019-05-12 16:54:23 +0200

    Import python project

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.6
+
+RUN pip install pipenv
+
+WORKDIR /app
+
+COPY ./Pipfile ./Pipfile
+COPY ./Pipfile.lock ./Pipfile.lock
+
+RUN pipenv install
+
+COPY . .
+
+CMD ["pipenv", "run", "python", "/app/main.py"]
---
+++ main.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+
+import os
+import argparse
+import sys
+import multiprocessing
+import csv
+
+import requests
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+
+parser = argparse.ArgumentParser(description='Runs a kmeans cluster analysis for anomaly detection')
+parser.add_argument("--url_in", help="remote address for the input data")
+parser.add_argument("--url_out", help="remote address for the output data")
+
+args = parser.parse_args()
+
+input = pd.read_csv(args.url_in)
+
+print(input)
+
+requests.post(args.url_out, data=input.to_csv())
